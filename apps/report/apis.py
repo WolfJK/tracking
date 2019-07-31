380e6e98c668fee31ffbda62e47c2c130560dd8c
letian <guoyuanpei12@sina.com>
2019-07-31 14:22:33 +0800

    报告解读

--- apps/report/apis.py
+++ apps/report/apis.py
@@ -2,20 +2,16 @@
 # __author__: 'GUO'
 from __future__ import unicode_literals
 
-from itertools import chain
-from operator import itemgetter
-from common.db_helper import DB
-from django.db.models import Q
 from common.models import *
 from datetime import datetime
 from django.db.models import F, Q, Func
 from common.db_helper import DB
-from StringIO import StringIO
 from io import BytesIO
 from dateutil.relativedelta import relativedelta
 import pandas
 from compiler import ast
 import json
+import pandas as pd
 
 
 report_affilication = {
@@ -207,6 +203,9 @@ def report_details(report_id):
                 dict(account="张三", platform="微博", post_count=33, user_type="BGC"),
                 dict(account="Edward", platform="宝宝树", post_count=2, user_type="KOL"),
                 dict(account="李四", platform="辣妈帮", post_count=1, user_type="水军"),
+                dict(account="李四", platform="辣妈帮", post_count=1, user_type="水军"),
+                dict(account="王五", platform="辣妈帮", post_count=1, user_type="水军"),
+                dict(account="张柳", platform="宝宝树", post_count=1, user_type="水军"),
             ],
 
             # 1.6. 投放声量趋势及账号趋势
@@ -384,57 +383,33 @@ def report_details(report_id):
     return data
 
 
-def get_unscramble(data):
-    '''
-    根据数据 获取解读结果
-    :param data:
-    :return:
-    '''
-    activity_max = max(data["spread_overview"]["activity"], key=lambda x: x["value"])
-    platform_max = max(ast.flatten([x["children"] for x in data["spread_overview"]["platform"]]), key=lambda x: x["value"])
-    platform_post_sum = sum([v["value"] for v in ast.flatten([x["children"] for x in data["spread_overview"]["platform"]])])
-
-
-    param = dict(
-        post_count=data["spread_overview"]["post_count"],
-        account_all=data["spread_overview"]["account_count"],
-        activity_count=len(data["spread_overview"]["activity"]),
-        activity_max=activity_max["name"],
-        activity_post_count=activity_max["value"],
-        platform_max=platform_max["name"],
-        platform_post_count=platform_max["value"],
-        platform_max_ratio=float(platform_max["value"] * 100.0 / platform_post_sum, 2),
-        account_max=float(platform_max["value"] * 100.0 / platform_post_sum, 2),
-    )
-
-
-
 unscramble_rule = dict(
     transmission=[
-        ("1", "1, 本次活动共投放{post_count}篇帖子，启用{account_all}个账号。\n 2, "),
+        ("1", "本次活动共投放{post_count}篇帖子，启用{account_all}个账号。\n 2, "),
         ("{activity_count} > 1", "{activity_max}主题活动投放声量最高（{activity_post_count}篇）；"),
         ("1", "活动主要在{platform_max}平台投放，共投放{platform_post_count}篇帖子，约占总投放声量的{platform_max_ratio}%。\n"),
-        ("{account_max} == {post_max}", "3, 从账号分布来看，{account_max}账号数量最多（{account_count}个），发布的帖子数也最多（{account_post_count}篇）\n"),
-        ("{account_max} != {post_max}", "3, 从账号分布来看，{account_max}账号数量最多（{account_count}个），而{account_post_max}账号发布帖子数最多（{account_post_count}篇）\n"),
+        ("{account_max} == {post_max}", "从账号分布来看，{account_max}账号数量最多（{account_count}个），发布的帖子数也最多（{account_post_count}篇）\n"),
+        ("{account_max} != {post_max}", "从账号分布来看，{account_max}账号数量最多（{account_count}个），而{account_post_max}账号发布帖子数最多（{account_post_count}篇）\n"),
 
     ],
     efficiency=[
         ("{platform_count} > 1 and {cb_platform_max} != {hd_platform_max}", "从平台角度看，本次活动在{cb_platform_max}的累计传播广度最高（{cb_platform_count}人次），而在{hd_platform_max}引发了最高的累计互动量（{hd_platform_count}人次）\n"),
         ("{platform_count} > 1 and {cb_platform_max} == {hd_platform_max}", "从平台角度看，本次活动在{cb_platform_max}引发了最高的累计互动量（{hd_platform_count}人次）和最高的累计传播广度（{cb_platform_count}人次）\n"),
-        ("{platform_count} == 1", "本次活动累计互动量{hd_platform_count}人次，累计传播广度{cb_platform_max}人次 \n"),
+        ("{platform_count} == 1", "本次活动累计互动量{hd_platform_count}人次，累计传播广度{cb_platform_count}人次 \n"),
+
         ("{hd_account_max} != {cb_account_max}", "从账号角度看，{cb_account_max}的发帖累计传播广度最高（{cb_account_count}人次），而{hd_account_max}的发帖引发了最高的累计互动量（{hd_account_count}人次） \n"),
         ("{hd_account_max} == {cb_account_max}", "从账号角度看，{cb_account_max}的发帖累计传播广度最高（{cb_account_count}人次），且引发了最高的累计互动量（{hd_account_count}人次）\n"),
 
         ("{hd_activity_max} != {cb_activity_max}", "从子活动角度看，“{cb_activity_max}”累计传播广度最高（{cb_activity_count}人次），而“{hd_activity_max}”引发了最高的累计互动量（{hd_activity_count}人次）\n"),
         ("{hd_activity_max} == {cb_activity_max}", "从子活动角度看，“{cb_activity_max}”累计传播广度最高（{cb_activity_count}人次），且引发了最高的累计互动量（{hd_activity_count}人次）\n"),
 
-        ("1", "互动大部分来源于{source}，约占整体的{source_ratio}%；评论中{account_comment}账号的占比最高，约为{account_comment_ratio}%。\n"),
+        ("1", "互动大部分来源于{source_max}，约占整体的{source_ratio}%；评论中{account_comment_max}账号的占比最高，约为{account_comment_ratio}%。\n"),
     ],
     effect_ugc=[
-        ("{activity_count} > 1", "本次活动期间UGC总计{ugc_count}人，其中活动UGC{activity_ugc_count}人，品牌UGC{brand_ugc_count}人。\n"),
-        ("1", "活动UGC中有{activity_brand_ugc_count}人（{activity_brand_ugc_ratio}%）提及品牌名称，增加了品牌声量；以#{activity_ugc_max}# 为主题的子活动引发的UGC人数最多，达{activity_ugc_count}个。\n"),
-        ("{activity_contribution} > {activity_contribution_pre}", "活动期内总计产生品牌UGC{brand_ugc_count}人。基于过往一年历史数据的预测，活动期内品牌UGC应该可以达到{brand_ugc_pre_count}，相差{brand_ugc_diff_count}人（-{brand_ugc_diff_ratio}%），没有达到预期效果\n"),
-        ("{activity_contribution} < {activity_contribution_pre}", "活动期内总计产生品牌UGC{brand_ugc_count}人。基于过往一年历史数据的预测，活动期内品牌UGC可以达到{brand_ugc_pre_count}人，活动为品牌贡献了{brand_ugc_diff_count}个（{brand_ugc_diff_ratio}%）UGC，超出预期效果\n"),
+        ("{activity_count} > 1", "本次活动期间UGC总计{ugc_count}人，其中活动UGC{activitys_ugc_count}人，品牌UGC{brands_ugc_count}人。\n"),
+        ("1", "活动UGC中有{activitys_brand_ugc_count}人（{activitys_brand_ugc_ratio}%）提及品牌名称，增加了品牌声量；以#{activity_ugc_max}# 为主题的子活动引发的UGC人数最多，达{activity_ugc_count}个。\n"),
+        ("{activitys_ugc_count} >= {brand_ugc_pre_count}", "活动期内总计产生品牌UGC{activitys_ugc_count}人。基于过往一年历史数据的预测，活动期内品牌UGC应该可以达到{brand_ugc_pre_count}，相差{brand_ugc_diff_count}人（-{brand_ugc_diff_ratio}%），没有达到预期效果\n"),
+        ("{activitys_ugc_count} < {brand_ugc_pre_count}", "活动期内总计产生品牌UGC{activitys_ugc_count}人。基于过往一年历史数据的预测，活动期内品牌UGC可以达到{brand_ugc_pre_count}人，活动为品牌贡献了{brand_ugc_diff_count}个（{brand_ugc_diff_ratio}%）UGC，超出预期效果\n"),
     ],
     effect_brand=[
         ("{brand_attention} > {brand_attention_year}", "活动期间内品牌关注度达{brand_attention}%，较年度关注度提升{brand_attention_ratio}%。\n"),
@@ -445,6 +420,99 @@ unscramble_rule = dict(
         ("{sales_point_cognitive} < {sales_point_cognitive_year}", "活动期内用户对于宣传卖点——“{sales_point}”的认知度为{sales_point_cognitive}%，较年度“{sales_point}”认知度下降{sales_point_cognitive_ratio}%\n"),
     ],
 )
+
+
+def get_unscramble(data):
+    '''
+    根据数据 获取解读结果
+    :param data:
+    :return:
+    '''
+    activity_max = max(data["spread_overview"]["activity"], key=lambda x: x["value"])
+    platform_max = max(ast.flatten([x["children"] for x in data["spread_overview"]["platform1"]]), key=lambda x: x["value"])
+    platform_post_sum = sum([v["value"] for v in ast.flatten([x["children"] for x in data["spread_overview"]["platform1"]])])
+    account_max_df = pd.DataFrame(data["spread_overview"]["account"])
+    account_max = account_max_df.drop_duplicates(subset=["account", "platform"]).groupby(["user_type"], as_index=False).agg({"account": pd.Series.nunique}).sort_values("account", ascending=False).iloc[0]
+    post_max = account_max_df.groupby(["user_type"], as_index=False).agg({"post_count": pd.Series.sum}).sort_values("post_count", ascending=False).iloc[0]
+
+    spread_efficiency = data["spread_efficiency"]["platform_cumulative"]
+    cb_platform_max = max(spread_efficiency, lambda x: x["breadth"])
+    hd_platform_max = max(spread_efficiency, lambda x: x["interaction"])
+
+    account_cumulative = data["spread_efficiency"]["account_cumulative"]
+    cb_account_max = max(account_cumulative, lambda x: x["breadth"])
+    hd_account_max = max(account_cumulative, lambda x: x["interaction"])
+
+    activity_cumulative = data["spread_efficiency"]["activity_cumulative"]
+    cb_activity_max = max(activity_cumulative, lambda x: x["breadth"])
+    hd_activity_max = max(activity_cumulative, lambda x: x["interaction"])
+
+    platform_cumulative = data["spread_efficiency"]["platform_cumulative"]
+    source_max = max(platform_cumulative, lambda x: x["value"])
+
+    user_type_composition = data["spread_efficiency"]["user_type_composition"]
+    account_comment_max = max(user_type_composition, lambda x: x["value"])
+
+    activity_ugc_max = data["spread_effectiveness"]["activity_ugc_in_activity_composition"]
+
+    param = dict(
+        post_count=data["spread_overview"]["post_count"],
+        account_all=data["spread_overview"]["account_count"],
+        activity_count=len(data["spread_overview"]["activity"]),
+        activity_max=activity_max["name"],
+        activity_post_count=activity_max["value"],
+        platform_max=platform_max["name"],
+        platform_post_count=platform_max["value"],
+        platform_max_ratio=float(platform_max["value"] * 100.0 / platform_post_sum, 2),
+        account_max=account_max.user_type,
+        account_count=account_max.account,
+        post_max=post_max.user_type,
+        account_post_max=post_max.post_count,
+
+        platform_count=len(spread_efficiency),
+        cb_platform_max=cb_platform_max["name"],
+        cb_platform_count=cb_platform_max["breadth"],
+        hd_platform_max=hd_platform_max["name"],
+        hd_platform_count=hd_platform_max["interaction"],
+
+        cb_account_max=cb_account_max["name"],
+        cb_account_count=cb_account_max["breadth"],
+        hd_account_max=hd_account_max["name"],
+        hd_account_count=hd_account_max["interaction"],
+
+        cb_activity_max=cb_activity_max["name"],
+        cb_activity_count=cb_activity_max["breadth"],
+        hd_activity_max=hd_activity_max["name"],
+        hd_activity_count=hd_activity_max["interaction"],
+
+        source_max=source_max["name"],
+        source_ratio=float(source_max["value"] * 100.0 / sum([x["value"] for x in platform_cumulative]), 2),
+
+        account_comment_max=source_max["name"],
+        account_comment_ratio=float(account_comment_max["value"] * 100.0 / sum([x["value"] for x in user_type_composition]), 2),
+
+        ugc_count=data["spread_effectiveness"]["ugc_count"],
+        activitys_ugc_count=data["spread_effectiveness"]["ugc_in_activity_count"],
+        brands_ugc_count=data["spread_effectiveness"]["ugc_mentioned_brand_count"],
+        activitys_brand_ugc_count=data["spread_effectiveness"]["ugc_intersect_count"],
+        activitys_brand_ugc_ratio=float(data["spread_effectiveness"]["activity_brand_ugc_count"] * 100.0 / data["spread_effectiveness"]["ugc_in_activity_count"], 2),
+        activity_ugc_max=activity_ugc_max["name"],
+        activity_ugc_count=activity_ugc_max["value"],
+
+        brand_ugc_pre_count=data["spread_effectiveness"]["predict"],
+        brand_ugc_diff_count=abs(data["spread_effectiveness"]["predict"] - data["spread_effectiveness"]["ugc_in_activity_count"]),
+        brand_ugc_diff_ratio=abs(float(data["spread_effectiveness"]["ugc_in_activity_count"] * 100.0 / data["spread_effectiveness"]["predict"], 2) - 1),
+
+        brand_attention=data["brand_concern"]["activity"],
+        brand_attention_year=data["brand_concern"]["annual"],
+        brand_attention_ratio=abs(float(data["brand_concern"]["activity"] - data["brand_concern"]["annual"] * 100, 2)),
+
+        sales_point_cognitive=data["tags_concern"]["activity"],
+        sales_point_cognitive_year=data["tags_concern"]["annual"],
+        sales_point="sales_point",
+        sales_point_cognitive_ratio=abs(float(data["tags_concern"]["activity"] - data["tags_concern"]["annual"] * 100, 2)),
+
+    )
 
 
 def report_config_create(param, user):
