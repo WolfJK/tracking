d223a2aa04fd8cf3a5190a07688eccab074034a0
letian <guoyuanpei12@sina.com>
2019-09-05 16:09:08 +0800

    精度

--- apps/report/apis.py
+++ apps/report/apis.py
@@ -243,6 +243,7 @@ def data_transform(data):
     apps_apis.set_precision(data["spread_effectiveness"]["brand_ugc_web"], keys=("value", "value_year"), precision=2)
     apps_apis.set_precision(data["spread_effectiveness"]["annual_average_trend"], keys=("value",), precision=2)
     apps_apis.set_precision(data["spread_effectiveness"], keys=("predict", "delta_absolute", "delta"), precision=0)
+    apps_apis.set_precision(data["spread_effectiveness"], keys=("delta"), precision=0, pct=100.1)
 
     apps_apis.set_precision(data["brand_concern"]["trend"], keys=("value", "value_year"), precision=2, pct=100.0)
     apps_apis.set_precision(data["brand_concern"], keys=("annual", "activity", "delta"), precision=2)
