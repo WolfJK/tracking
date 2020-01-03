7340e05f0ff4b7f759b2bb98c8eb259cc2d8af0e
letian <guoyuanpei12@sina.com>
2020-01-03 14:44:01 +0800

    初始化 品牌

--- script/app/update_script.sql
+++ script/app/update_script.sql
@@ -92,3 +92,92 @@ insert into sm_role_menus(smrole_id, smmenu_id) values (2, 5);
 insert into sm_role_menus(smrole_id, smmenu_id) values (2, 6);
 insert into sm_role_menus(smrole_id, smmenu_id) values (2, 7);
 
+
+
+
+-- ---------------- dim_brand 初始化 脚本
+-- truncate dim_brand_tmp;   【先导入 数据到 dim_brand_tmp 临时表】
+drop table if exists dim_brand_load_tmp;
+create table dim_brand_load_tmp
+(
+    id                  int auto_increment primary key,
+    tagName             varchar(128)  null,
+    keyword             varchar(512)  null,
+    saas_keyword        varchar(5120) null,
+    saas_exclude        varchar(5120) null,
+    saas_exclude_remark varchar(5120) null,
+    brand_2             varchar(64)   null,
+    brand_3             varchar(64)   null,
+    brand_1             varchar(64)   null,
+    parent_id           int           null,
+    saas                varchar(32)   null comment 'saas 的品牌名',
+    category            varchar(64)   null
+)
+;
+
+-- 1、更新字段
+update dim_brand_load_tmp set
+    brand_1 = regexp_substr(tagName, '[^.]+', 1, 2),
+    brand_2 = regexp_substr(tagName, '[^.]+', 1, 3),
+    brand_3 = regexp_substr(tagName, '[^.]+', 1, 4),
+    category = case
+    when regexp_substr(tagName, '[^.]+', 1, 1) = '奶粉品牌' then '奶粉'
+    when regexp_substr(tagName, '[^.]+', 1, 1) = '咖啡品牌' then '咖啡'
+    end
+;
+
+-- 2、去除 二级 和三级, 一级 和二级重复
+update dim_brand_load_tmp set brand_3 = null where brand_2 is not null and brand_3 is not null and brand_2 = brand_3;
+update dim_brand_load_tmp set brand_2 = null where brand_1 is not null and brand_2 is not null and brand_1 = brand_2;
+
+
+-- 3、去重
+drop table if exists dim_brand_deduplication_tmp;
+create table dim_brand_deduplication_tmp as
+select
+    brand_1, brand_2, brand_3, max(keyword) as keyword, max(parent_id) as parent_id, max(saas) as saas,
+    max(category) as category, max(saas_keyword) as saas_keyword, max(saas_exclude) as saas_exclude, max(saas_exclude_remark) as saas_exclude_remark
+from dim_brand_load_tmp group by brand_1, brand_2, brand_3
+;
+
+-- 添加主键
+ALTER TABLE dim_brand_deduplication_tmp ADD id int NOT NULL PRIMARY KEY AUTO_INCREMENT;
+
+
+-- 3、加工 saas
+update dim_brand_deduplication_tmp set saas = case
+    when brand_1 is not null and brand_2 is not null and brand_3 is not null then concat(brand_1, '.', brand_2, '.', brand_3)
+    when brand_1 is not null and brand_2 is not null then concat(brand_1, '.', brand_2)
+    else brand_1
+    end;
+
+-- 4、加工 parent_id
+update dim_brand_deduplication_tmp t1, dim_brand_deduplication_tmp t2
+    set t1.parent_id = t2.id
+where
+    t1.brand_1 is not null and t1.brand_2 is not null and t1.brand_3 is not null
+and t2.brand_1 is not null and t2.brand_2 is not null and t2.brand_3 is null
+and t1.brand_1 = t2.brand_1 and t1.brand_2 = t2.brand_2
+;
+
+update dim_brand_deduplication_tmp t1, dim_brand_deduplication_tmp t2
+    set t1.parent_id = t2.id
+where
+    t1.brand_1 is not null and t1.brand_2 is not null and t1.brand_3 is null
+and t2.brand_1 is not null and t2.brand_2 is null and t2.brand_3 is null
+and t1.brand_1 = t2.brand_1
+;
+
+
+-- 5、生成 dim_brand 表
+insert into dim_brand(id, name, parent_id, saas, keyword, saas_keyword, saas_exclude, saas_exclude_remark)
+select id, brand_3 as name, parent_id, saas, keyword, saas_keyword, saas_exclude, saas_exclude_remark from dim_brand_deduplication_tmp where brand_3 is not null
+union
+select id, brand_2 as name, parent_id, saas, keyword, saas_keyword, saas_exclude, saas_exclude_remark from dim_brand_deduplication_tmp where brand_3 is null and brand_2 is not null
+union
+select id, brand_1 as name, parent_id, saas, keyword, saas_keyword, saas_exclude, saas_exclude_remark from dim_brand_deduplication_tmp where brand_3 is null and brand_2 is null and brand_1 is not null
+;
+-- 6、更新 品牌 品类表
+
+insert into dim_brand_category(brand_id, category_id) select id, 1 from dim_brand_deduplication_tmp where category = '奶粉';
+insert into dim_brand_category(brand_id, category_id) select id, 3 from dim_brand_deduplication_tmp where category = '咖啡';
